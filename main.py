48bf9aaaf1594d1b3ce77a1a94f334366989e49c
jonnrauber <natanrauber97@gmail.com>
2018-11-05 22:17:20 -0200

    new port configuration for heroku setup

--- main.py
+++ main.py
@@ -77,4 +77,5 @@ def logout():
     return redirect(url_for('pg_index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081, debug=True)
+    port = int(os.environ.get("PORT", 8081))
+    app.run(host='0.0.0.0', port=port, debug=True)
