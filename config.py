41df7000858445470a66b424baadf9f56c032e99
Jonathan Rauber <natanrauber97@gmail.com>
2018-11-06 19:50:16 -0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-SECRET_KEY = 'sports'
+SECRET_KEY = os.environ['SECRET_KEY']
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
