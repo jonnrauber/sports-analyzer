e50474bd7ee5acc17ede54451d54a7fe702e4015
aluno-uffs <Aluno UFFS>
2018-11-06 19:41:17 -0200

    get DATABASE_URL from environment

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 SECRET_KEY = 'sports'
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/sportsanalyzer'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
